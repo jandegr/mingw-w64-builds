7bdd149ac20085fa30629f4040c1c9a5c1aeb794
jandegr <jandegr@users.noreply.github.com>
2019-06-08 17:58:00 +0200

    Update build-zlib-1.2.8.sh

--- build-zlib-1.2.8.sh
+++ build-zlib-1.2.8.sh
@@ -20,11 +20,6 @@ pushd ${ARCHIVE_DIR} > /dev/null
 curl --retry 5 --remote-name -L ${URL} || exit 1
 popd > /dev/null
 
-pushd ${SOURCE_DIR} > /dev/null
-if [ -d ${SOURCE_DIR_NAME} ]
-then
-    rm -rf ${SOURCE_DIR_NAME}
-fi
 
 gunzip ${ARCHIVE_DIR}/${ARCHIVE_NAME} || exit 1
 tar xf ${ARCHIVE_DIR}/${TARBALL_NAME} || exit 1
